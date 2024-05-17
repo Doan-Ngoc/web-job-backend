9c4dad03fdc7bed271cc8916c302c5350479ac98
Doan-Ngoc <minhngocdoan3112@gmail.com>
2024-05-17 17:12:05 +0700

    Initial state

--- middlewares/ValidationHandler.js
+++ middlewares/ValidationHandler.js
@@ -2,7 +2,6 @@ const { validationResult } = require("express-validator");
 
 // sequential processing, stops running validations chain if the previous one fails.
 const validate = async (req, res, next) => {
-  console.log('reach here')
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log('error', errors)
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,11 +4,13 @@ const authCtr = require("../controllers/auth.ctr");
 const userValidation = require("../middlewares/validations/user.validation");
 const loginValidation = require("../middlewares/validations/login.validation");
 const validate = require("../middlewares/ValidationHandler");
+const requireSignin = require("../middlewares/requireSignin")
 
 const router = Router();
 
 router.post("/signup", userValidation, validate, authCtr.createUser);
 router.post("/signin", loginValidation, validate, authCtr.login);
+router.post("/token/verify", requireSignin)
 router.post("/token", authCtr.refreshToken);
 
 module.exports = router;
